1b2166094d7271d67b459fabd2ded98dd69bdddc
Tom Beauregard <thomas.beauregard@gmail.com>
2017-08-24 15:01:41 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 console.log("Up and running!")
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne =  cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("sorry, try again.");
+}
+
+
