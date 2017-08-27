e4fe83dcf0c1c17e7344ede5657c4e2c2f7aa352
Tom Beauregard <thomas.beauregard@gmail.com>
2017-08-27 16:32:20 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ var cards = [
 	{
 		rank: "king",
 		suit: "diamonds",
-		cardImage: "images/king-of-diamonds"
+		cardImage: "images/king-of-diamonds.png"
 	}
 ];
 
@@ -25,7 +25,7 @@ var cardsInPlay = [];
 
 var checkForMatch = function() 
 {
-
+	
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
       	alert("You found a match!");
@@ -35,17 +35,30 @@ var checkForMatch = function()
 	}
 }
 
-var flipCard = function(cardId) 
+var flipCard = function() 
 {
+	cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("user flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function()
+{
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
 
 
 
